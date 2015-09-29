16f2c26f094ae24129b58178be07a19416f93a31
Renan <renanmiguel93@hotmail.com>
2015-09-29 02:04:06 -0300

    commit inicial

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+#include "lista.h"
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+    iniciar();
+
+    return 0;
+}
+
---
+++ pcb.h
@@ -0,0 +1,8 @@
+#ifndef PCB_H_
+#define PCB_H_
+
+typedef struct {
+    char name[30];
+    int priority;
+
+} pcb;
